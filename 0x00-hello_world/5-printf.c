21e4664e30e756e10068c7100b055aba093c4eac
yaraaahmed <yaraa5258@gmail.com>
2023-06-15 17:42:30 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * description:A c programme that prints with put function
+ * Return:always 0 (sucsses)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
