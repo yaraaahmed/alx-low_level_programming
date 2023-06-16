c75eff2293d38e2f3689cc19e280201e0ec20f79
yaraaahmed <yaraa5258@gmail.com>
2023-06-15 19:17:48 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description:print a quote using write function
+ * ssize_t write (int fd, const void *buf , size_t count);
+ * Return: 1 (error)
+*/
+int main(void)
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quo, 59);
+	return (1);
+}
+
