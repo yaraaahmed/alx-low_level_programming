17de209231c77de3e6060d1f5e4cf981be122c05
yaraaahmed <yaraa5258@gmail.com>
2023-06-18 05:38:35 -0700

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,7 +21,7 @@ int main(void)
 				putchat((seconDigit / 10) + 48);
 				putchar((seconDigit % 10) + 48);
 
-				if (firstDigit != 98 || seconDigit != 99)
+				if (firstDigit + seconDigit != 197)
 				{
 					putchar(',');
 					putchar(' ');
