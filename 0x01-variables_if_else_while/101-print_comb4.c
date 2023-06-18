3c1dcfed2694ab48a1593ed9cbba467dd5e834f2
yaraaahmed <yaraa5258@gmail.com>
2023-06-18 05:15:07 -0700

    comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description:program that prints different combinations of two digits.
+ * Return:always 0 (sucsses)
+*/
+int main(void)
+{
+	int digit1 = 0;
+	int digit2, digit3;
+
+	while (digit1 <= 9)
+	{
+		digit2 = 0;
+		while (digit2 <= 9)
+		{
+			digit3 = 0;
+			while (digit3 <= 9)
+			{
+				if (digit1 != digit2 &&
+					digit1 < digit2 &&
+					digit2 != digit3 &&
+					digit2 < digit3)
+				{
+					putchar(digit1 + 48);
+					putchar(digit2 + 48);
+					putchar(digit3 + 48);
+
+					if (digit1 + digit2 + digit3 != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				digit3++;
+			}
+			digit2++;
+		}
+		digit1++;
+	}
+	putchar('\n');
+
+	return (0);
+}
