b6bd3dd441f1280e2d17565bfdef3d47f38669d8
yaraaahmed <yaraa5258@gmail.com>
2023-06-20 12:09:18 -0700

    time_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,19 +21,20 @@ void print_times_table(int n)
 
 				prod = num * mult;
 
-				if (prod <= 9)
-					_putchar(' ');
 				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
 					_putchar(' ');
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + 48);
 					_putchar((prod / 10) % 10 + 48);
-				} else if (prod <= 99 && prod >= 10)
+				} 
+				else if (prod <= 99 && prod >= 10)
 					_putchar((prod / 10) + 48);
-				_putchar((prod % 10) + 48);
 			}
-			_putchar('\n');
+				_putchar((prod % 10) + 48)
 		}
+		_putchar('\n');
 	}
 }
