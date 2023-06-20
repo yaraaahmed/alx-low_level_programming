7c30af180f06aa5a8414ff612fd8fa3822df6481
yaraaahmed <yaraa5258@gmail.com>
2023-06-20 14:14:16 -0700

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,9 +13,9 @@ int main(void)
 
 	for (num = 0; num < 1024; num++)
 	{
-		if ((num % 3 == 0) || (num % 5 == 0)
+		if ((num % 3 == 0) || (num % 5 == 0))
 			sum += num;
-			}
+		}
 			printf("%d\n", sum);
 			return (0);
 }
