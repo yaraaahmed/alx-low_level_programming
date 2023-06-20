2f86c11d06f5f15f65495e27b100e5577a0219c9
yaraaahmed <yaraa5258@gmail.com>
2023-06-20 12:10:22 -0700

    table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -33,7 +33,7 @@ void print_times_table(int n)
 				else if (prod <= 99 && prod >= 10)
 					_putchar((prod / 10) + 48);
 			}
-				_putchar((prod % 10) + 48)
+				_putchar((prod % 10) + 48);
 		}
 		_putchar('\n');
 	}
