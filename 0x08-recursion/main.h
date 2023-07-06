0d259997189ce07dca3825efd2bd3a94fb989643
yaraaahmed <yaraa5258@gmail.com>
2023-07-05 17:36:18 -0700

    wildcmp_strl

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,6 +10,8 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int actual_sqrt_recursion(int n, int i);
 int actual_prime(int n, int i);
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 #endif
