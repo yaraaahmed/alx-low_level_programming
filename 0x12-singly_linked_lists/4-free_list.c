94bf7bcecb81989bff16923941171dbe587fff18
yaraaahmed <yaraa5258@gmail.com>
2023-07-29 06:41:39 -0700

    free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - free linked
+ * @head: freed list
+*/
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
